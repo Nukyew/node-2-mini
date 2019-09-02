14866ba6c6f8b160a05bf57784bd78d3f33d3e49
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-09-02 16:16:00 -0600

    fixed update/delete issues, project finished

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -15,15 +15,23 @@ module.exports = {
         res.status(200).send(books)
     },
     update: (req, res, next) => {
-        books.splice(req.params.id, 1, {
-            id: parseInt(req.params.id, 10),
-            title: req.body.title,
-            author: req.body.author
-        })
+        for (let prop in books){
+            if (books[prop].id === parseInt(req.params.id, 10)) {
+                books.splice(books.indexOf(books[prop]), 1, {
+                    id: parseInt(req.params.id, 10),
+                    title: req.body.title,
+                    author: req.body.author 
+                })
+            }
+        }
         res.status(200).send(books)
     },
     delete: (req, res, next) => {
-        books.splice(req.params.id, 1)
+        for (let prop in books){
+            if (books[prop].id === parseInt(req.params.id, 10)){
+                books.splice(books.indexOf(books[prop]), 1)
+            }
+        }
         res.status(200).send(books)
     }
 }
